59caca5669a798d5bf6972abb0c73bc403b0f44e
wumj <648326357@qq.com>
2019-06-14 11:28:31 +0800

    push table.

--- guangde/sql/oracle_create_table.sql
+++ guangde/sql/oracle_create_table.sql
@@ -1,5 +1,5 @@
 -- Create table
-create table ATTACHMENT
+create table TB_ATTACHMENT
 (
   id         VARCHAR2(32) not null,
   relationid VARCHAR2(32) not null,
@@ -9,20 +9,20 @@ create table ATTACHMENT
   filename   VARCHAR2(100)
 );
 -- Add comments to the table 
-comment on table ATTACHMENT is '附件信息表';
+comment on table TB_ATTACHMENT is '附件信息表';
 -- Add comments to the columns 
-comment on column ATTACHMENT.id is 'id';
-comment on column ATTACHMENT.relationid is '关联id';
-comment on column ATTACHMENT.type is '0-用户头像';
-comment on column ATTACHMENT.path is '路径';
-comment on column ATTACHMENT.filetype is '文件类型';
-comment on column ATTACHMENT.filename is '文件名称';
+comment on column TB_ATTACHMENT.id is 'id';
+comment on column TB_ATTACHMENT.relationid is '关联id';
+comment on column TB_ATTACHMENT.type is '0-用户头像';
+comment on column TB_ATTACHMENT.path is '路径';
+comment on column TB_ATTACHMENT.filetype is '文件类型';
+comment on column TB_ATTACHMENT.filename is '文件名称';
 --  Create/Recreate primary, unique and foreign key constraints 
-alter table ATTACHMENT add constraint PK_ATTACHMENT primary key (ID);
+alter table TB_ATTACHMENT add constraint PK_ATTACHMENT primary key (ID);
 
   
  -- Create table
-create table USERS
+create table TB_USERS
 (
   userid        VARCHAR2(32) not null,
   nickname      VARCHAR2(128),
@@ -41,24 +41,24 @@ create table USERS
   iseffective   NUMBER(2) default 0
 );
 -- Add comments to the table 
-comment on table USERS is '注册会员用户表';
+comment on table TB_USERS is '注册会员用户表';
 -- Add comments to the columns 
-comment on column USERS.nickname is '昵称';
-comment on column USERS.password is '密码';
-comment on column USERS.fullname is '全称';
-comment on column USERS.email is '邮箱';
-comment on column USERS.mobile is '手机';
-comment on column USERS.fromcity is '城市';
-comment on column USERS.sign is '签名';
-comment on column USERS.gender is '性别 0-男，1-女';
-comment on column USERS.validateemail is '邮箱是否认证 0-否 1-是';
-comment on column USERS.validateuser is '用户是否认证 0-否 1-是';
-comment on column USERS.starts is '可用积分';
-comment on column USERS.validateinfo is '认证信息';
-comment on column USERS.createdate is '创建时间';
-comment on column USERS.iseffective is '是否有效用户0-有效，1-无效';
+comment on column TB_USERS.nickname is '昵称';
+comment on column TB_USERS.password is '密码';
+comment on column TB_USERS.fullname is '全称';
+comment on column TB_USERS.email is '邮箱';
+comment on column TB_USERS.mobile is '手机';
+comment on column TB_USERS.fromcity is '城市';
+comment on column TB_USERS.sign is '签名';
+comment on column TB_USERS.gender is '性别 0-男，1-女';
+comment on column TB_USERS.validateemail is '邮箱是否认证 0-否 1-是';
+comment on column TB_USERS.validateuser is '用户是否认证 0-否 1-是';
+comment on column TB_USERS.starts is '可用积分';
+comment on column TB_USERS.validateinfo is '认证信息';
+comment on column TB_USERS.createdate is '创建时间';
+comment on column TB_USERS.iseffective is '是否有效用户0-有效，1-无效';
 -- Create/Recreate primary, unique and foreign key constraints 
-alter table USERS
+alter table TB_USERS
   add constraint PK_USER primary key (USERID);
 
   
